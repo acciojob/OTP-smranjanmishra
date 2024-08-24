b37036e832d35f00f598c5e897e16948ecb7183f
Soumya Ranjan Mishra <118365110+smranjanmishra@users.noreply.github.com>
2024-08-24 21:23:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+codes.forEach((code, idx) => {
+    code.addEventListener('input', () => {
+        if (code.value.length > 0 && idx < codes.length - 1) {
+            codes[idx + 1].focus();
+        }
+    });
+
+    code.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace') {
+            if (code.value.length === 0 && idx > 0) {
+                codes[idx - 1].focus();
+            }
+        }
+    });
+});
